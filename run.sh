1a8a69f3fd38314d641906215ab3c578c4edd4bb
Jonathan Walker <walkjo53@gmail.com>
2021-09-26 03:37:28 -0700

    initial commit

---
+++ run.sh
@@ -0,0 +1,1 @@
+elm-live src/Main.elm -- --output=public/main.js
---
+++ src/Main.elm
@@ -0,0 +1,70 @@
+module Main exposing (..)
+
+import Browser
+import Browser.Navigation as Nav
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Url
+
+-- main
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+-- model
+type alias Model =
+    { key : Nav.Key
+    , url : Url.Url
+    }
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( Model key url, Cmd.none )
+
+-- update
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url }
+            , Cmd.none
+            )
+
+-- subs
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+-- view
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Elm Browser Template"
+    , body =
+        [ div []
+            [ span [] [ text "Elm Browser Template 0.0.0" ]
+            ]
+        ]
+    }
+
+viewLink : String -> Html msg
+viewLink path =
+    li [] [ a [ href path ] [ text path ] ]
